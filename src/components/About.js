6c09d7c0d4fef4846e179f151d62a780651d6325
ktkyletran <ktkyletran@gmail.com>
2021-08-07 12:10:59 -0500

    added about component

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import kyle from '../images/kyle.png'
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <section className="about-img">
+                <img src={kyle} alt="kyle" />
+            </section>
+            <section className="about-desc">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit perferendis quas, voluptas, delectus dolorem incidunt iste autem, quo culpa quasi assumenda saepe soluta quis? Qui, impedit tempora! Eaque quia dolor deleniti soluta quos. Rem autem delectus eos ullam, harum fugit obcaecati eum dolores incidunt optio praesentium voluptate, cupiditate asperiores quam?
+            </section>
+        </div>
+    )
+}
+
+export default About
