b13f93d4fff6454b633a194d38248eeef8acfba8
ktkyletran <ktkyletran@gmail.com>
2021-08-07 12:10:16 -0500

    added landing page and mobile css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './App.css';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <About />
+      <div className="brand">
+        <a href="/" className="brand-about">about</a>
+        <h1 className="brand-name">r.diate</h1>
+        <h4 className="brand-subtitle">jewelry</h4>
+        <a href="/" className="brand-products">products</a>
+      </div>
     </div>
   );
 }
